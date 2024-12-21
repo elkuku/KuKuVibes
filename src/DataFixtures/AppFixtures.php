f530146278476431c5fffffe5dbd4d2a00077307
elkuku <der.el.kuku@gmail.com>
2024-12-21 12:05:50 -0500

    Fix tests (?)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Feed;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,16 +13,26 @@ class AppFixtures extends Fixture
     #[\Override]
     public function load(ObjectManager $manager): void
     {
-        $manager->persist(
-            (new User())
-                ->setIdentifier('user')
-        );
+        $user = (new User())
+            ->setIdentifier('user');
+        $manager->persist($user);
 
         $manager->persist(
             (new User())
                 ->setIdentifier('admin')
                 ->setRoles([User::ROLES['admin']])
         );
+
+        $category = (new Category())
+            ->setName('TEST');
+        $manager->persist($category);
+
+$manager->persist((new Feed())
+            ->setName('TEST')
+->setUrl('TEST.com')
+->setCategory($category)
+->setOwner($user));
+
 
         $manager->flush();
     }
